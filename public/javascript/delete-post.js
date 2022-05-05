4106f9bf7c4ff94faa5b8d9b7dc6c909bfe7f6bf
Madison Kendall <lilyzetakovic@gmail.com>
2022-05-05 13:30:46 -0600

    created deleteFormHandler function

---
+++ public/javascript/delete-post.js
@@ -0,0 +1,23 @@
+// function to delete a post
+async function deleteFormHandler(event) {
+  event.preventDefault();
+
+  // get the post_id of post user wants to delete
+  const post_id = window.location.toString().split("/")[
+    window.location.toString().split("/").length - 1
+  ];
+
+  // defining the method as DELETE
+  const response = await fetch("/api/posts/#{post_id}", {
+    method: "DELETE"
+  });
+
+  if (response.ok) {
+    document.location.replace("/dashboard");
+  } else {
+    alert(response.statusText);
+  }
+}
+
+// event listener for the delete button
+document.querySelector(".delete-post-btn").addEventListener("click", deleteFormHandler);
