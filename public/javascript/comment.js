1f60392d7edf9356eeb515d9109622f1c0a52ebc
Madison Kendall <lilyzetakovic@gmail.com>
2022-05-05 13:20:31 -0600

    made the commentFormHandler function

---
+++ public/javascript/comment.js
@@ -0,0 +1,34 @@
+// function for submission of a new comment
+// capture the form content
+async function commentFormHandler(event) {
+  event.preventDefault();
+
+  const comment_text = document.querySelector('textarea[name="comment-body"]').value.trim();
+
+  const post_id = window.location.toString().split("/")[
+  window.location.toString().split("/").length -1
+];
+
+// defining the method as a POST
+if (comment_text) {
+  const response = await fetch("/api/comments", {
+    method: "POST",
+    body: JSON.stringify({
+      post_id,
+      comment_text
+    }),
+    headers: {
+      "Content-Type": "application/json"
+    }
+  });
+
+  if (response.ok) {
+    document.location.reload();
+  } else {
+    alert(response.statusText);
+  }
+}
+}
+
+// this is the event listener for submission of a new comment
+document.querySelector(".comment-form").addEventListener("submit", commentFormHandler);
