3058895ca7059755f679735d26d8c50a2e972511
Madison Kendall <lilyzetakovic@gmail.com>
2022-05-11 14:01:31 -0600

    found typo in edit-post

--- public/javascript/edit-post.js
+++ public/javascript/edit-post.js
@@ -4,7 +4,7 @@ async function editFormHandler(event) {
 
   // capture the title, content, and id from the post being edit
   const title = document.querySelector('input[name="post-title"]').value;
-  const post_content = document.querySelector('textarea[name="post_content"]').value().trim();
+  const post_content = document.querySelector('textarea[name="post-content"]').value().trim();
   const post_id = window.location.toString().split("/")[
     window.location.toString().split("/").length - 1
   ];
